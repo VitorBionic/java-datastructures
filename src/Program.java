ecc4217c8545d79289d86b21ffa88d9369e68fe5
Vitor Bionic <vitorbionic@hotmail.com>
2024-09-23 12:33:13 -0300

    first commit

---
+++ src/Program.java
@@ -0,0 +1,17 @@
+public class Program {
+	public static void main(String[] args) {
+		char[] arr = {'j', 'b', 'v', 'd', 'r', 'a'};
+		insertionSort(arr);
+	}
+	
+	public static void insertionSort(char[] a) {
+		int n = a.length;
+		for (int i = 1; i < n; i++) {
+			char cur = a[i];
+			int j = i - 1;
+			while ((j >= 0) && (a[j] > cur))
+				a[j + 1] = a[j--];
+			 a[j + 1] = cur;
+		}
+	}
+}
---
+++ src/Teste.java
@@ -0,0 +1,15 @@
+
+public class Teste {
+
+	public static void main(String[] args) {
+		char[] arr = {'b', 'j', 'v', 'v', 'r', 'a'};
+		char cur = 'd';
+		int j = 1;
+		if ((j >= 0) && (arr[j] > cur)) {
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
+	}
+
+}
