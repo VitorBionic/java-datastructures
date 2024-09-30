254b6d5e4563f7520cbe2b44dfffa1e5fa9742d0
Vitor Bionic <vitorbionic@hotmail.com>
2024-09-30 15:06:16 -0300

    Unordered Array implementation added

---
+++ src/array/unorderedarray/Program.java
@@ -0,0 +1,22 @@
+package array.unorderedarray;
+
+public class Program {
+	public static void main(String[] args) {
+		UnorderedArray<Integer> ua = new UnorderedArray<Integer>(5);
+		
+		ua.insert(15);
+		ua.insert(2);
+		ua.insert(70);
+		ua.insert(69);
+		ua.insert(3);
+		
+		System.out.println(ua);
+		System.out.println(ua.search(69));
+		System.out.println(ua.get(3));
+		
+		ua.remove(3);
+		
+		System.out.println(ua.checkMemory());
+	}
+
+}
---
+++ src/array/unorderedarray/UnorderedArray.java
@@ -0,0 +1,83 @@
+package array.unorderedarray;
+
+// Creating class
+public class UnorderedArray <T> {
+
+	// Instance  Variables
+	private int capacity;
+	private int lastPosition;
+	private T[] elements;
+	
+	// Instance block(or block)
+	{
+		lastPosition = -1; // executed when a object is instantiated in scope inside it, accessing its own instance variables
+	}
+	
+	// Constructor of class
+	public UnorderedArray(int capacity) {
+		this.capacity = capacity;
+		this.elements = (T[]) new Object[capacity];
+	}
+	
+	// Method get - Just to recover the values of the private instance variable elements
+	public T get(int index) {
+		if (index > lastPosition) {
+			throw new ArrayIndexOutOfBoundsException("Index " + index + " is out of bounds for length " + (lastPosition + 1));
+		}
+		return elements[index];
+	}
+	
+	// Method insert
+	// O(1)
+	public void insert(T element) {
+		if (lastPosition == capacity - 1) {
+			throw new ArrayIndexOutOfBoundsException("Array is Full");
+		} else {
+			lastPosition++;
+			elements[lastPosition] = element;
+		}
+	}
+	
+	// Method search
+	// O(n)
+	public int search(T value) {
+		for (int i = 0; i <= lastPosition; i++) {
+			if (value.equals(elements[i]))
+				return i;
+		}
+		return -1;
+	}
+	
+	// Method remove
+	// O(n)
+	public void remove(T value) {
+		int index = search(value);
+		for (int i = index; i < lastPosition; i++)
+			elements[i] = elements[i + 1];
+		elements[lastPosition] = null;
+		lastPosition--;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("[");
+		if (lastPosition > -1)
+		    sb.append(elements[0]);
+		for (int i = 1; i <= lastPosition; i++)
+			sb.append(", " + elements[i]);
+		sb.append("]");
+		return sb.toString();			
+	}
+	
+	// Didactic method to see all values in array elements, including the elements not controlled by the lastPosition variable
+	public String checkMemory() {
+		StringBuilder sb = new StringBuilder("[");
+		if (lastPosition > -1)
+		    sb.append(elements[0]);
+		for (int i = 1; i < capacity; i++)
+			sb.append(", " + elements[i]);
+		sb.append("]");
+		return sb.toString();
+	}
+	
+}
